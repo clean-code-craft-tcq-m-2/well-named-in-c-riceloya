72015ba2a12cd8100930b40dd9f957c0b4139d23
riceloya <109813509+riceloya@users.noreply.github.com>
2022-11-08 13:04:11 -0600

    commit_1:  reduction of codespace

--- color_test.h
+++ color_test.h
@@ -8,7 +8,16 @@ const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
-} ColorPair
+} ColorPair;
+
 extern int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 extern int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+ 
+/*extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
+extern ColorPair GetColorFromPairNumber(int pairNumber);
+extern int GetPairNumberFromColor(const ColorPair* colorPair);
+extern int GetPairNumberFromColor(const ColorPair* colorPair);
+extern void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+extern void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+*/
--- main.c
+++ main.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
 #include "color_test.h"
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);
-}
-
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);}
 ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair;
-    int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = 
-        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor =
-        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    ColorPair colorPair; int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
     return colorPair;
 }
-
-int GetPairNumberFromColor(const ColorPair* colorPair) {
-    return colorPair->majorColor * numberOfMinorColors +
-            colorPair->minorColor + 1;
-}
-
+int GetPairNumberFromColor(const ColorPair* colorPair) {return colorPair->majorColor * numberOfMinorColors +colorPair->minorColor + 1;}
 void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor)
 {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
@@ -30,7 +18,6 @@ void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorCol
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
 }
-
 void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
 {
     ColorPair colorPair;
@@ -40,28 +27,24 @@ void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPa
     printf("Got pair number %d\n", pairNumber);
     assert(pairNumber == expectedPairNumber);
 }
-
 void print_color_code(){
-    char *buffer;
-    char *title= "///////////////////25 COLOR CODE MANUAL///////////////////";
-    char *k_iter= "Pair Number: ";
-    char *M_Color= "Major color: ";
-    char *m_Color= "|| Minor color: ";
-    char k =0;
-    const ColorPair ColorPair;
-    FILE *Manual = fopen("manual_c.txt", "w");
+    char *buffer;char *title= "///////////////////25 COLOR CODE MANUAL///////////////////";
+    char *k_iter= "Pair Number: "; char *M_Color= "Major color: "; char *m_Color= "|| Minor color: ";
+    char k =0;const ColorPair ColorPair; FILE *Manual = fopen("manual_c.txt", "w");
     fprintf(Manual,"%s\n", title, buffer);
     for (int i = WHITE; i < MAX_MAJOR; i++){
         for (int j = BLUE; j < MAX_MINOR; j++){
-            //printf("%d, %d, %s, %d, %s\n", k, i, MajorColorNames[i],  j, MinorColorNames[j]);
             fprintf(Manual,"%s %d %s %d %s %s  %d %s\n",k_iter, k,M_Color, i, MajorColorNames[i],m_Color,  j, MinorColorNames[j], buffer);
             k++;
         }
     }
     fclose(Manual);
 }
-
 int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
     print_color_code();
     return 0;
 }
