58b0044c07c044af58d68a34a41ef545416c64d7
riceloya <109813509+riceloya@users.noreply.github.com>
2022-11-08 14:22:52 -0600

    commit_2 multiple files packaging and warning removal

--- color_test.h
+++ color_test.h
@@ -1,23 +1,23 @@
+#ifndef color_test
+#define color_test
 #include <stdio.h>
 #include <assert.h>
+#define MAX_COLORPAIR_NAME_CHARS  16
 
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET, MAX_MAJOR};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE, MAX_MINOR};
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+
 typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
 
-extern int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-extern int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
- 
-/*extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
-extern ColorPair GetColorFromPairNumber(int pairNumber);
-extern int GetPairNumberFromColor(const ColorPair* colorPair);
-extern int GetPairNumberFromColor(const ColorPair* colorPair);
-extern void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
-extern void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
-*/
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
+
+#endif
--- main.c
+++ main.c
@@ -1,40 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
 #include "color_test.h"
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {sprintf(buffer, "%s %s", MajorColorNames[colorPair->majorColor], MinorColorNames[colorPair->minorColor]);}
-ColorPair GetColorFromPairNumber(int pairNumber) {
-    ColorPair colorPair; int zeroBasedPairNumber = pairNumber - 1;
-    colorPair.majorColor = (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-    colorPair.minorColor = (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-    return colorPair;
-}
-int GetPairNumberFromColor(const ColorPair* colorPair) {return colorPair->majorColor * numberOfMinorColors +colorPair->minorColor + 1;}
-void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor)
-{
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-    assert(colorPair.majorColor == expectedMajor);
-    assert(colorPair.minorColor == expectedMinor);
-}
-void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
+
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+
 void print_color_code(){
-    char *buffer;char *title= "///////////////////25 COLOR CODE MANUAL///////////////////";
-    char *k_iter= "Pair Number: "; char *M_Color= "Major color: "; char *m_Color= "|| Minor color: ";
-    char k =0;const ColorPair ColorPair; FILE *Manual = fopen("manual_c.txt", "w");
-    fprintf(Manual,"%s\n", title, buffer);
+    char *title= "///////////////////25 COLOR CODE MANUAL///////////////////";
+    char *k_iter= "Pair Number: "; 
+    char *M_Color= "Major color: ";
+    char *m_Color= "|| Minor color: ";
+    char k =0;
+    FILE *Manual = fopen("manual_c.txt", "w");
+    fprintf(Manual,"%s\n", title);
     for (int i = WHITE; i < MAX_MAJOR; i++){
         for (int j = BLUE; j < MAX_MINOR; j++){
-            fprintf(Manual,"%s %d %s %d %s %s  %d %s\n",k_iter, k,M_Color, i, MajorColorNames[i],m_Color,  j, MinorColorNames[j], buffer);
+            fprintf(Manual,"%s %d %s %d %s %s  %d %s\n",k_iter, k, M_Color, i, MajorColorNames[i], m_Color,  j, MinorColorNames[j]);
             k++;
         }
     }
